b06a738875cb8f986be233a89d092b0eb74c8b03
tete <orayasangpheng@gmail.com>
2021-05-14 18:55:11 +0700

    ADD place search & place service

---
+++ lib/services/places_service.dart
@@ -0,0 +1,15 @@
+import 'package:ToiletPocket/models/place_search.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert; 
+
+class PlacesService{
+  final key = 'AIzaSyAvRknAjBrBfPCXrBTeNYJ8j8g6Fq32frQ';
+
+  Future<List<PlaceSearch>> getAutocomplete(String search) async{
+    var url = 'https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$search&types=(cities)&key=AIzaSyAvRknAjBrBfPCXrBTeNYJ8j8g6Fq32frQ';
+    var reponse = await http.get(url);
+    var json = convert.jsonDecode(reponse.body);
+    var jsonResults = json['predirections'] as List;
+    return jsonResults.map((place) => PlaceSearch.fromJson(place)).toList();
+  }
+}
