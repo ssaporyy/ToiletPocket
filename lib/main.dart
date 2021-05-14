c6700600bcffc7d553735e3ed125e3cc699ee141
tete <orayasangpheng@gmail.com>
2021-05-14 18:54:09 +0700

    Edit new main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:ToiletPocket/searchbar.dart';
+import 'package:ToiletPocket/src/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:ToiletPocket/map.dart';
+import 'package:provider/provider.dart';
+import 'blocs/application_bloc.dart';
 
 
 void main() {
@@ -11,42 +14,48 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Toilet Pocket',
-      // Start the app with the "/" named route. In this case, the app starts
-      // on the FirstScreen widget.
-      initialRoute: '/',
-      routes: {
-        // When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => MapSample(),
-        // When navigating to the "/second" route, build the SecondScreen widget.
-        // '/second': (context) => (),
-        'third': (context) => SearchBar(),
-      },
+    return ChangeNotifierProvider(
+      create: (context) => ApplicationBloc(),
+      child: MaterialApp(
+        title: 'Toilet Pocket',
+        theme: ThemeData(
+          primarySwatch: Colors.amber,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
+        // initialRoute: '/',
+        // routes: {
+        //   // When navigating to the "/" route, build the FirstScreen widget.
+        //   '/': (context) => MapSample(),
+          // When navigating to the "/second" route, build the SecondScreen widget.
+          // '/second': (context) => (),
+          // 'third': (context) => SearchBar(),
+        // },
+      ),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // title: Text('Login'),
-      ),
-      // body: Center(
-      //   child: ElevatedButton(
-      //     // Within the `FirstScreen` widget
-      //     onPressed: () {
-      //       // Navigate to the second screen using a named route.
-      //       Navigator.pushNamed(context, '/second');
-      //     },
-      //     child: Text('Launch screen'),
-      //   ),
-      // ),
-    );
-  }
-}
+// class FirstScreen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // title: Text('Login'),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           // Within the `FirstScreen` widget
+//           onPressed: () {
+//             // Navigate to the second screen using a named route.
+//             Navigator.pushNamed(context, '/second');
+//           },
+//           child: Text('Launch screen'),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // class SecondScreen extends StatelessWidget {
 //   @override
