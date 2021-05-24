68c08c5a876899d46e00172dcec055dac1e95329
tete <orayasangpheng@gmail.com>
2021-05-24 13:05:58 +0700

    UPDATE current map unfinish

--- lib/map.dart
+++ lib/map.dart
@@ -1,46 +1,117 @@
 // import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:outline_material_icons/outline_material_icons.dart';
-import 'package:location/location.dart';
+// import 'package:location/location.dart';
 
 class MapSample extends StatefulWidget {
   @override
   State<MapSample> createState() => MapSampleState();
 }
 
-Future<LocationData> getCurrentLocation() async {
-  Location location = Location();
-  try {
-    return await location.getLocation();
-  } on PlatformException catch (e) {
-    if (e.code == 'PERMISSION_DENIED') {
-      // Permission denied
-    }
-    return null;
-  }
-}
+// Future<LocationData> getCurrentLocation() async {
+//   Location location = Location();
+//   try {
+//     return await location.getLocation();
+//   } on PlatformException catch (e) {
+//     if (e.code == 'PERMISSION_DENIED') {
+//       // Permission denied
+//     }
+//     return null;
+//   }
+// }
+
+
 
 class MapSampleState extends State<MapSample> {
+  static const _intitialCameraPosition = CameraPosition(
+    target: LatLng(13.7650836, 100.5379664),
+    zoom: 16,
+    );
+
   GoogleMapController _googleMapController;
-  LocationData currentLocation;
+  Position position;
+  Widget _child;
 
-  Future _goToMe() async {
-    final GoogleMapController controller = await _googleMapController;
-    currentLocation = await getCurrentLocation();
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
-      target: LatLng(currentLocation.latitude, currentLocation.longitude),
-      zoom: 16,
-    )));
-  }
 
-  @override
-  void dispose() {
-    _googleMapController.dispose();
-    super.dispose();
-  }
+  // Future<void> getLocation() async {
+  //   PermissionStatus permission = await PermissionHandler()
+  //       .checkPermissionStatus(PermissionGroup.location);
 
+  //   if (permission == PermissionStatus.denied) {
+  //     await PermissionHandler()
+  //         .requestPermissions([PermissionGroup.locationAlways]);
+  //   }
+
+  //   var geolocator = Geolocator();
+
+  //   GeolocationStatus geolocationStatus =
+  //       await geolocator.checkGeolocationPermissionStatus();
+
+  //   switch (geolocationStatus) {
+  //     case GeolocationStatus.denied:
+  //       showToast('denied');
+  //       break;
+  //     case GeolocationStatus.disabled:
+  //       showToast('disabled');
+  //       break;
+  //     case GeolocationStatus.restricted:
+  //       showToast('restricted');
+  //       break;
+  //     case GeolocationStatus.unknown:
+  //       showToast('unknown');
+  //       break;
+  //     case GeolocationStatus.granted:
+  //       showToast('Access granted');
+  //       _getCurrentLocation();
+  //   }
+  // }
+
+
+  // LocationData currentLocation;
+
+  // Future _goToMe() async {
+  //   final GoogleMapController controller = await _googleMapController;
+  //   currentLocation = await getCurrentLocation();
+  //   controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+  //     target: LatLng(currentLocation.latitude, currentLocation.longitude),
+  //     zoom: 16,
+  //   )));
+  // }
+
+  // @override
+  // void dispose() {
+    // _googleMapController.dispose();
+    // super.dispose();
+  // }
+
+  // void locatePosition() async{
+  // Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  // currentPosition = position;
+
+  // LatLng latLatPosition = LatLng(position.latitude, position.longitude);
+
+  // CameraPosition cameraPosition = new CameraPosition(target: latLatPosition, zoom:14);
+  // _googleMapController.animateCamera(
+                // CameraUpdate.newCameraPosition(cameraPosition));
+
+
+// }
+
+// @override
+//   void initState() {
+//     getLocation();
+//     super.initState();
+//   }
+//   void _getCurrentLocation() async{
+//     Position res = await Geolocator().getCurrentPosition();
+//     setState(() {
+//       position = res;
+//       _child = _mapWidget();
+//     });
+//   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,10 +122,11 @@ class MapSampleState extends State<MapSample> {
             myLocationButtonEnabled: true,
             myLocationEnabled: true,
             zoomControlsEnabled: false,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(13.7650836, 100.5379664),
-              zoom: 16,
-            ),
+            
+      //       initialCameraPosition:CameraPosition(
+      //   target: LatLng(position.latitude,position.longitude),
+      //   zoom: 12.0,
+      // ),
             onMapCreated: (controller) => _googleMapController = controller,
           ),
 
@@ -68,7 +140,9 @@ class MapSampleState extends State<MapSample> {
               backgroundColor: Colors.white.withOpacity(0.8),
               //Theme.of(context).primaryColorLight,
               foregroundColor: Colors.black,
-              onPressed: _goToMe,
+              // onPressed: _goToMe,
+              onPressed : () => _googleMapController.animateCamera(
+                CameraUpdate.newCameraPosition(_intitialCameraPosition)),
               child: Icon(
                 Icons.my_location,
                 size: 20,
