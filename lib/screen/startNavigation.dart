abdf033a10bdf0c4288b6ddeac54b2463496fec1
tete <orayasangpheng@gmail.com>
2021-10-26 23:36:19 +0700

    UPDATE start navigate

--- lib/screen/startNavigation.dart
+++ lib/screen/startNavigation.dart
@@ -29,6 +29,59 @@ class _NavigationState extends State<Navigation> {
     super.initState();
   }
 
+  Widget _textField({
+   TextEditingController controller,
+   FocusNode focusNode,
+   String label,
+   String hint,
+   double width,
+   Icon prefixIcon,
+   Widget suffixIcon,
+   Function(String) locationCallback,
+  }) {
+    return Container(
+      width: width * 0.8,
+      child: TextField(
+        onChanged: (value) {
+          locationCallback(value);
+        },
+        controller: controller,
+        // focusNode: focusNode,
+        // enabled: false,
+        readOnly: true,
+        decoration: new InputDecoration(
+          prefixIcon: prefixIcon,
+          suffixIcon: suffixIcon,
+          labelText: label,
+          filled: true,
+          fillColor: Colors.white,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            borderSide: BorderSide(
+              color: Colors.grey.shade400,
+              width: 2,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+            borderSide: BorderSide(
+              color: Colors.blue.shade300,
+              width: 2,
+            ),
+          ),
+          contentPadding: EdgeInsets.all(15),
+          hintText: hint,
+        ),
+      ),
+    );
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     Completer<GoogleMapController> _mapController = Completer();
@@ -36,6 +89,10 @@ class _NavigationState extends State<Navigation> {
     final _args =
         ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
     final _place = _args['places'] as Places;
+    Set<Marker> markers = {};
+    var width = MediaQuery.of(context).size.width;
+    final startAddressController = GoogleMapController;
+
     return Scaffold(
         backgroundColor: ToiletColors.colorBackground,
         body:
@@ -57,63 +114,138 @@ class _NavigationState extends State<Navigation> {
                       onMapCreated: (GoogleMapController controller) {
                         _mapController.complete(controller);
                       },
+                      markers: Set<Marker>.of(markers),
                     ),
                   ),
-                  Align(
-                    alignment: Alignment.topCenter,
-                    child: Container(
-                      padding: EdgeInsets.only(left: 7),
-                      height: 130,
-                      width: double.maxFinite,
+                  SafeArea(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white70,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                    width: width * 0.9,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
                       child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: TextButton(
-                                  style: TextButton.styleFrom(
-                                    alignment: Alignment.center,
-                                    primary: Colors.black,
-                                    padding: EdgeInsets.all(5),
-                                    textStyle: const TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.w600,
-                                      fontFamily: 'Sukhumvit' ?? 'SF-Pro',
-                                    ),
-                                  ),
-                                  onPressed: () {
-                                    // Navigator.pop(context);
-                                    Navigator.pushNamed(context, '/two');
-                                  },
-                                  child: Row(
-                                    children: <Widget>[
-                                      Icon(
-                                        Icons.arrow_back_ios_rounded,
-                                        size: 18,
-                                        color: Colors.blue,
-                                      ),
-                                      Text(
-                                        'กลับ',
-                                        style: TextStyle(
-                                          color: Colors.blue,
-                                          fontSize: 15.0,
-                                          fontFamily: 'Sukhumvit' ?? 'SF-Pro',
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Text(
+                            'Places',
+                            style: TextStyle(fontSize: 20.0),
                           ),
+                          SizedBox(height: 10),
+                          _textField(
+                              label: 'Start',
+                              hint: 'wasan',
+                              
+                              prefixIcon: Icon(Icons.looks_one),
+                              suffixIcon: IconButton(
+                                icon: Icon(Icons.my_location),
+                                // onPressed: () {
+                                //   startAddressController = _currentAddress;
+                                //   _startAddress = _currentAddress;
+                                // },
+                              ),
+                              // controller: startAddressController,
+                              // focusNode: startAddressFocusNode,
+                              width: width,
+                              locationCallback: (String value) {
+                                setState(() {
+                                  // _startAddress = value;
+                                });
+                              }),
+                          SizedBox(height: 10),
+                          _textField(
+                              label: 'Destination',
+                              hint: 'Choose destination',
+                              prefixIcon: Icon(Icons.looks_two),
+                              // controller: destinationAddressController,
+                              // focusNode: desrinationAddressFocusNode,
+                              width: width,
+                              locationCallback: (String value) {
+                                setState(() {
+                                  // _destinationAddress = value;
+                                });
+                              }),
+                          SizedBox(height: 10),
+                          // Visibility(
+                          //   visible: _placeDistance == null ? false : true,
+                          //   child: Text(
+                          //     'DISTANCE: $_placeDistance km',
+                          //     style: TextStyle(
+                          //       fontSize: 16,
+                          //       fontWeight: FontWeight.bold,
+                          //     ),
+                          //   ),
+                          // ),
+                          SizedBox(height: 5),
+                          // ElevatedButton(
+                          //   onPressed: (_startAddress != '' &&
+                          //           _destinationAddress != '')
+                          //       ? () async {
+                          //           startAddressFocusNode.unfocus();
+                          //           desrinationAddressFocusNode.unfocus();
+                          //           setState(() {
+                          //             if (markers.isNotEmpty) markers.clear();
+                          //             if (polylines.isNotEmpty)
+                          //               polylines.clear();
+                          //             if (polylineCoordinates.isNotEmpty)
+                          //               polylineCoordinates.clear();
+                          //             _placeDistance = null;
+                          //           });
+
+                          //           _calculateDistance().then((isCalculated) {
+                          //             if (isCalculated) {
+                          //               ScaffoldMessenger.of(context)
+                          //                   .showSnackBar(
+                          //                 SnackBar(
+                          //                   content: Text(
+                          //                       'Distance Calculated Sucessfully'),
+                          //                 ),
+                          //               );
+                          //             } else {
+                          //               ScaffoldMessenger.of(context)
+                          //                   .showSnackBar(
+                          //                 SnackBar(
+                          //                   content: Text(
+                          //                       'Error Calculating Distance'),
+                          //                 ),
+                          //               );
+                          //             }
+                          //           });
+                          //         }
+                          //       : null,
+                          //   child: Padding(
+                          //     padding: const EdgeInsets.all(8.0),
+                          //     child: Text(
+                          //       'Show Route'.toUpperCase(),
+                          //       style: TextStyle(
+                          //         color: Colors.white,
+                          //         fontSize: 20.0,
+                          //       ),
+                          //     ),
+                          //   ),
+                          //   style: ElevatedButton.styleFrom(
+                          //     primary: Colors.red,
+                          //     shape: RoundedRectangleBorder(
+                          //       borderRadius: BorderRadius.circular(20.0),
+                          //     ),
+                          //   ),
+                          // ),
                         ],
                       ),
                     ),
                   ),
+                ),
+              ),
+            ),
+                  
                   Align(
                     alignment: Alignment.bottomCenter,
                     child: Container(
@@ -241,6 +373,61 @@ class _NavigationState extends State<Navigation> {
                                 ),
                               ],
                             ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      padding: EdgeInsets.only(left: 7),
+                      height: 130,
+                      width: double.maxFinite,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: TextButton(
+                                  style: TextButton.styleFrom(
+                                    alignment: Alignment.center,
+                                    primary: Colors.black,
+                                    padding: EdgeInsets.all(5),
+                                    textStyle: const TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600,
+                                      fontFamily: 'Sukhumvit' ?? 'SF-Pro',
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    // Navigator.pop(context);
+                                    Navigator.pushNamed(context, '/two');
+                                  },
+                                  child: Row(
+                                    children: <Widget>[
+                                      Icon(
+                                        Icons.arrow_back_ios_rounded,
+                                        size: 18,
+                                        color: Colors.blue,
+                                      ),
+                                      Text(
+                                        'กลับ',
+                                        style: TextStyle(
+                                          color: Colors.blue,
+                                          fontSize: 15.0,
+                                          fontFamily: 'Sukhumvit' ?? 'SF-Pro',
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
